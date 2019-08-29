db7c7e3dc4620209797a9bd1ab9598686cd508bc
Caglar Cavdar <chaglare@gmail.com>
2019-08-28 21:16:22 -0500

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 pipeline{
     agent any
-    properties([parameters([string(defaultValue: 'plan', description: 'plan/apply', name: 'USER_ACTION', trim: true)])])
+    parameters {string(defaultValue: "plan", description: "plan/apply", name: 'USER_ACTION')}
     stages{
         stage("Run Command"){
             steps{
@@ -68,7 +68,7 @@ pipeline{
         stage("Clone VPC Repo"){
             steps{
                 ws("terraform/"){
-                    git "https://github.com/farrukh90/infrastructure_april.git"
+                    git "https://github.com/chaglare/infrastructure.git"
                 }
             }
         }
